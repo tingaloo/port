f687c5bf8ee37bebf47b1f8670e280f9bb4d811b
Ting Lew <ting.lew@gmail.com>
2015-01-07 14:45:05 -0500

    Created book index

--- bookgarden/app/controllers/books_controller.rb
+++ bookgarden/app/controllers/books_controller.rb
@@ -1,4 +1,32 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+  end
+
+  def new
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    @book.save
+    redirect_to books_path
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+
+  end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author)
   end
 end
