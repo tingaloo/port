8f6547a53d9d5b009c26298f5f530b454c041990
Ting Lew <ting.lew@gmail.com>
2015-01-07 13:21:54 -0500

    Set up book controller and gems

--- redditdiy/app/controllers/application_controller.rb
+++ redditdiy/app/controllers/application_controller.rb
@@ -2,4 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # redirects to current page after signing in from header.
+  # Located in devise docs.
+  def after_sign_in_path_for(resource)
+    request.env['omniautho.origin'] || stored_location_for(resource) || root_path
+  end
+
+  # Updates cannot be saved without explicitly PERMITTING PARAMETERS
+  # Which is how rails deals with mass assignment, so for every new assignment,
+  # We add them to the permitted parameters assignment
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
+            devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email,
+              :password, :current_password) }
+        end
 end
--- redditdiy/config/routes.rb
+++ redditdiy/config/routes.rb
@@ -1,56 +1,23 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  get 'static_pages/documentation'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  # Need both comment resources
+  resources :comments
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  devise_for :users
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  # Learn proper nesting, comments are nested in posts.
+  resources :posts do
+    resources :comments
+    member do
+      put "like", to: "posts#upvote"
+      put "dislike", to: "posts#downvote"
+    end
+  end
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  root "posts#index" # Shortcut to the posts index under view>posts>index.html.erb
+  # also displays all the posts.
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
